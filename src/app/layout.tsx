21ceef805844ff962b6d2f038a26308e759a7726
slyang08 <e2223845@gmail.com>
2026-04-08 16:38:47 -0400

    feat: add Header for the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/ui/header/header";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
@@ -96,7 +97,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className="flex min-h-full flex-col">
+        <Header />
         {children}
 
         <Script
