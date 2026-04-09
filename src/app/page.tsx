75d24b0f7596dc4128dde193301f68c653e5c729
slyang08 <e2223845@gmail.com>
2026-04-09 01:32:41 -0400

    feat: add projects page for the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 // src/app/page.tsx
 import { About } from "@/features/about";
+import { Projects } from "@/features/projects";
 
 export default function Home() {
   return (
-    <section id="about" className="mx-auto max-w-5xl px-4 py-20">
-      <About />
-    </section>
+    <>
+      <section id="about" className="mx-auto max-w-5xl px-4 py-20">
+        <About />
+      </section>
+      <section id="projects" className="mx-auto max-w-5xl px-4 py-20">
+        <Projects />
+      </section>
+    </>
   );
 }
