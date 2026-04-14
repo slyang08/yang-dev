b036dcb468af3cf9d7f376f8f617608c45ea2ebf
slyang08 <e2223845@gmail.com>
2026-04-13 21:01:18 -0400

    docs: add demo pic to display on page

--- src/features/projects/ProjectCard.tsx
+++ src/features/projects/ProjectCard.tsx
@@ -4,35 +4,44 @@ import type { Project } from "./projects.data";
 
 export function ProjectCard({ name, description, image, github, demo, tags }: Project) {
   return (
-    <div className="rounded-xl border p-4 transition-all duration-300 hover:-translate-y-1 hover:shadow-xl">
-      {image && <Image src={image} alt={name} className="mb-3 rounded-md" />}
-
-      <h3 className="text-lg font-semibold">{name}</h3>
-
-      <p className="text-muted-foreground mt-2 text-sm">{description}</p>
-
-      {tags && (
-        <div className="mt-3 flex flex-wrap gap-2">
-          {tags.map((tag) => (
-            <span key={tag} className="bg-muted rounded px-2 py-1 text-xs">
-              {tag}
-            </span>
-          ))}
-        </div>
+    <>
+      {image && (
+        <Image
+          src={image.src}
+          alt={image.alt}
+          width={200}
+          height={200}
+          className="mb-3 h-50 w-50 rounded-md"
+        />
       )}
+      <div className="rounded-xl border p-4 transition-all duration-300 hover:-translate-y-1 hover:shadow-xl">
+        <h3 className="text-lg font-semibold">{name}</h3>
 
-      <div className="mt-4 flex gap-4 text-sm">
-        {github && (
-          <a href={github} target="_blank" rel="noopener noreferrer" className="underline">
-            GitHub
-          </a>
+        <p className="text-muted-foreground mt-2 text-sm">{description}</p>
+
+        {tags && (
+          <div className="mt-3 flex flex-wrap gap-2">
+            {tags.map((tag) => (
+              <span key={tag} className="bg-muted rounded px-2 py-1 text-xs">
+                {tag}
+              </span>
+            ))}
+          </div>
         )}
-        {demo && (
-          <a href={demo} target="_blank" rel="noopener noreferrer" className="underline">
-            Live Demo
-          </a>
-        )}
+
+        <div className="mt-4 flex gap-4 text-sm">
+          {github && (
+            <a href={github} target="_blank" rel="noopener noreferrer" className="underline">
+              GitHub
+            </a>
+          )}
+          {demo && (
+            <a href={demo} target="_blank" rel="noopener noreferrer" className="underline">
+              Live Demo
+            </a>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/features/projects/projects.data.ts
+++ src/features/projects/projects.data.ts
@@ -4,7 +4,11 @@ export type Project = {
   id: string;
   name: string;
   description: string;
-  image?: string;
+  image?: {
+    name: string;
+    src: string;
+    alt: string;
+  };
   github: string;
   demo?: string;
   highlights?: string[];
@@ -17,7 +21,11 @@ export const projects: Project[] = [
     name: "LiuShuShu",
     description:
       "Built this project with Node.js (Express), Next.js, and PostgreSQL for a Malaysian durian retailer",
-    // image: "/projects/portfolio.png",
+    image: {
+      name: "LiuShuShu phone Demo",
+      src: "/lss-demo-phone.png",
+      alt: "LiuShuShu Demo",
+    },
     github: "https://github.com/slyang08/LiuShuShu",
     demo: "https://liushushu.vercel.app",
     tags: ["TypeScript", "Node.js", "Express", "Next.js", "PostgreSQL", "Tailwind CSS"],
@@ -28,7 +36,13 @@ export const projects: Project[] = [
     description: `Built a full-stack personal finance tracking system with Node.js, Express, and MongoDB.
                   Implemented CRUD operations, data persistence, and responsive UI with React.
                   Focused on improving user experience and efficient data management.`,
+    image: {
+      name: "FinTrackEasy laptop Demo",
+      src: "/fte-demo-laptop.png",
+      alt: "FinTrackEasy Demo",
+    },
     github: "https://github.com/Project-Implementation-PRJ666/FinTrackEasy",
+    demo: "https://fintrackeasy.vercel.app/",
     highlights: [
       "Implemented JWT authentication",
       "Designed relational database schema",
